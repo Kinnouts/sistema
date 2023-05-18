21ddd39f1bb4c14d527711224666c0d3c54693aa
Mariana <kinnouts@gmail.com>
2023-05-18 11:54:45 -0300

    Creacion de repo

---
+++ ajax/articulo.php
@@ -0,0 +1,70 @@
+<?php 
+require_once "../modelos/Articulo.php";
+
+$articulo=new Articulo();//Diez atributos instanciar
+$idarticulo=isset($_POST["idarticulo"])? limpiarCadena($_POST["idarticulo"]):"";
+$idcategoria=isset($_POST["idcategoria"])? limpiarCadena($_POST["idcategoria"]):"";
+$codigo=isset($_POST["codigo"])? limpiarCadena($_POST["nombcodigore"]):"";
+$nombre=isset($_POST["nombre"])? limpiarCadena($_POST["nombre"]):"";
+$stock=isset($_POST["stock"])? limpiarCadena($_POST["stock"]):"";
+$imagen=isset($_POST["imagen"])? limpiarCadena($_POST["imagen"]):"";
+$condicion=isset($_POST["condicion"])? limpiarCadena($_POST["condicion"]):"";
+
+
+$descripcion=isset($_POST["descripcion"])? limpiarCadena($_POST["descripcion"]):"";
+
+switch ($_GET["op"]){
+	case 'guardaryeditar':
+		if (empty($idarticulo)){
+			$rspta=$articulo->insertar($nombre,$descripcion);
+			echo $rspta ? "Articulo registrado" : "Articulo no se pudo registrar";
+		}
+		else {
+			$rspta=$articulo->editar($idarticulo,$nombre,$descripcion);
+			echo $rspta ? "Articulo actualizado" : "Articulo no se pudo actualizar";
+		}
+	break;
+
+	case 'desactivar':
+		$rspta=$articulo->desactivar($idarticulo);
+ 		echo $rspta ? "Categoría Desactivada" : "Categoría no se puede desactivar";
+ 	break;
+
+	case 'activar':
+		$rspta=$articulo->activar($idarticulo);
+ 		echo $rspta ? "Categoría activada" : "Categoría no se puede activar";
+ 	break;
+
+	case 'mostrar':
+		$rspta=$articulo->mostrar($idarticulo);
+ 		//Codificar el resultado utilizando json
+ 		echo json_encode($rspta);
+ 	break;
+
+	case 'listar':
+		$rspta=$categoria->listar();
+ 		//Vamos a declarar un array
+ 		$data= Array();
+
+ 		while ($reg=$rspta->fetch_object()){
+ 			$data[]=array(
+ 				"0"=>($reg->condicion)?'<button class="btn btn-warning" onclick="mostrar('.$reg->idcategoria.')"><i class="fa fa-pencil"></i></button>'.
+ 					' <button class="btn btn-danger" onclick="desactivar('.$reg->idcategoria.')"><i class="fa fa-close"></i></button>':
+ 					'<button class="btn btn-warning" onclick="mostrar('.$reg->idcategoria.')"><i class="fa fa-pencil"></i></button>'.
+ 					' <button class="btn btn-primary" onclick="activar('.$reg->idcategoria.')"><i class="fa fa-check"></i></button>',
+ 				"1"=>$reg->nombre,
+ 				"2"=>$reg->descripcion,
+ 				"3"=>($reg->condicion)?'<span class="label bg-green">Activado</span>':
+ 				'<span class="label bg-red">Desactivado</span>'
+ 				);
+ 		}
+ 		$results = array(
+ 			"sEcho"=>1, //Información para el datatables
+ 			"iTotalRecords"=>count($data), //enviamos el total registros al datatable
+ 			"iTotalDisplayRecords"=>count($data), //enviamos el total registros a visualizar
+ 			"aaData"=>$data);
+ 		echo json_encode($results);
+
+	break;
+}
+?>
---
+++ config/global.php
@@ -0,0 +1,13 @@
+<?php
+//IP de la PC servidor de BD
+define ("DB_HOST","localhost");
+//nombre de la bd
+define("DB_NAME","dbsistema");
+//USuario de la BD
+define("DB_USERNAME", "root");
+//Contraseña de usuario de la bd
+define("DB_PASSWORD","");
+//Definimos codificacion de caracteres
+define("DB_ENCODE","utf8");
+define("PRO_NAME", "ITVentas");
+?>
