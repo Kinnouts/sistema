174c982abfef09922249debc7f701015d2ce5ec1
Mariana <kinnouts@gmail.com>
2023-06-12 06:51:17 -0300

    con permiso y art completo

--- vistas/articulo.php
+++ vistas/articulo.php
@@ -1,4 +1,4 @@
-<?php  
+<?php 
 require 'header.php';
 ?>
 <!--Contenido-->
@@ -10,7 +10,7 @@ require 'header.php';
               <div class="col-md-12">
                   <div class="box">
                     <div class="box-header with-border">
-                          <h1 class="box-title">Articulo <button class="btn btn-success" id="btnagregar" onclick="mostrarform(true)"><i class="fa fa-plus-circle"></i> Agregar</button></h1>
+                          <h1 class="box-title">Artículo <button class="btn btn-success" id="btnagregar" onclick="mostrarform(true)"><i class="fa fa-plus-circle"></i> Agregar</button> <a target="_blank" href="../reportes/rptarticulos.php"><button class="btn btn-info">Reporte</button></a> </h1>
                         <div class="box-tools pull-right">
                         </div>
                     </div>
@@ -21,8 +21,8 @@ require 'header.php';
                           <thead>
                             <th>Opciones</th>
                             <th>Nombre</th>
-                            <th>Categoria</th>
-                            <th>Codigo</th>
+                            <th>Categoría</th>
+                            <th>Código</th>
                             <th>Stock</th>
                             <th>Imagen</th>
                             <th>Estado</th>
@@ -30,32 +30,53 @@ require 'header.php';
                           <tbody>                            
                           </tbody>
                           <tfoot>
-                          <th>Opciones</th>
+                            <th>Opciones</th>
                             <th>Nombre</th>
-                            <th>Categoria</th>
-                            <th>Codigo</th>
+                            <th>Categoría</th>
+                            <th>Código</th>
                             <th>Stock</th>
                             <th>Imagen</th>
                             <th>Estado</th>
                           </tfoot>
                         </table>
                     </div>
-                    <div class="panel-body" style="height: 400px;" id="formularioregistros">
+                    <div class="panel-body" id="formularioregistros">
                         <form name="formulario" id="formulario" method="POST">
-                            
-                            <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <label>Nombre(*):</label>
-                                <input type="hidden" name="id articulo" id="idarticulo">
-                                <input type="text" class="form-control" name="nombre" id="nombre"maxlength="100" placeholder="Nombre" required>
+                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Nombre(*):</label>
+                            <input type="hidden" name="idarticulo" id="idarticulo">
+                            <input type="text" class="form-control" name="nombre" id="nombre" maxlength="100" placeholder="Nombre" required>
+                          </div>
+                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Categoría(*):</label>
+                            <select id="idcategoria" name="idcategoria" class="form-control selectpicker" data-live-search="true" required></select>
+                          </div>
+                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Stock(*):</label>
+                            <input type="number" class="form-control" name="stock" id="stock" required>
+                          </div>
+                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Descripción:</label>
+                            <input type="text" class="form-control" name="descripcion" id="descripcion" maxlength="256" placeholder="Descripción">
+                          </div>
+                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Imagen:</label>
+                            <input type="file" class="form-control" name="imagen" id="imagen">
+                            <input type="hidden" name="imagenactual" id="imagenactual">
+                            <img src="" width="150px" height="120px" id="imagenmuestra">
+                          </div>
+                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Código:</label>
+                            <input type="text" class="form-control" name="codigo" id="codigo" placeholder="Código Barras">
+                            <button class="btn btn-success" type="button" onclick="generarbarcode()">Generar</button>
+                            <button class="btn btn-info" type="button" onclick="imprimir()">Imprimir</button>
+                            <div id="print">
+                              <svg id="barcode"></svg>
                             </div>
-                            <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <label>Categoria(*):</label>
-                                <select id="idcategoria" name="idcategoria" class="form-control selectpicker" data-live-search="true" required></select>
-                            </div>
-
+                          </div>
                           <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
                             <button class="btn btn-primary" type="submit" id="btnGuardar"><i class="fa fa-save"></i> Guardar</button>
-
+ 
                             <button class="btn btn-danger" onclick="cancelarform()" type="button"><i class="fa fa-arrow-circle-left"></i> Cancelar</button>
                           </div>
                         </form>
@@ -65,10 +86,12 @@ require 'header.php';
               </div><!-- /.col -->
           </div><!-- /.row -->
       </section><!-- /.content -->
-
+ 
     </div><!-- /.content-wrapper -->
   <!--Fin-Contenido-->
 <?php
 require 'footer.php';
 ?>
+<script type="text/javascript" src="../public/js/JsBarcode.all.min.js"></script>
+<script type="text/javascript" src="../public/js/jquery.PrintArea.js"></script>
 <script type="text/javascript" src="scripts/articulo.js"></script>
---
+++ vistas/scripts/permiso.js
@@ -0,0 +1,56 @@
+var tabla;
+
+//Función que se ejecuta al inicio
+function init(){
+	mostrarform(false);
+	listar();
+
+}
+
+function mostrarform(flag)
+{
+	
+	if (flag)
+	{
+		$("#listadoregistros").hide();
+		$("#formularioregistros").show();
+		$("#btnGuardar").prop("disabled",false);
+		$("#btnagregar").hide();
+	}
+	else
+	{
+		$("#listadoregistros").show();
+		$("#formularioregistros").hide();
+		$("#btnagregar").hide();//Va a entrar siempre por el else
+	}
+}
+
+
+function listar()
+{
+	tabla=$('#tbllistado').dataTable(
+	{
+		"aProcessing": true,//Activamos el procesamiento del datatables
+	    "aServerSide": true,//Paginación y filtrado realizados por el servidor
+	    dom: 'Bfrtip',//Definimos los elementos del control de tabla
+	    buttons: [	//Le estádando funcionalidad a cada botón según la librería	          
+		            'copyHtml5',
+		            'excelHtml5',
+		            'csvHtml5',
+		            'pdf'
+		        ],
+		"ajax":
+				{
+					url: '../ajax/permiso.php?op=listar',
+					type : "get",
+					dataType : "json",						
+					error: function(e){
+						console.log(e.responseText);	
+					}
+				},
+		"bDestroy": true,
+		"iDisplayLength": 5,//Paginación
+	    "order": [[ 0, "desc" ]]//Ordenar (columna,orden)
+	}).DataTable();
+}
+init();
